28a96073660efdcf6049a6dbce4bc4bddd8d3d4d
dragos.pantiru <dragos.pantiru@asf.ro>
2020-05-06 09:21:54 +0300

    added login/signup tmp

---
+++ src/main/java/com/oeco/controllers/dto/auth/UserDTO.java
@@ -0,0 +1,53 @@
+package com.oeco.controllers.dto.auth;
+
+import org.springframework.data.annotation.Id;
+
+public class UserDTO {
+    @Id
+    public String id;
+    public String fullName;
+    public String email;
+    public String password;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDTO{" +
+                "id='" + id + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
